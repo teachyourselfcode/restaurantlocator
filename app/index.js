5beb2765702bfdba7f11fec5b2c08007bb84a98d
Coby Durling <Coby Durling>
2017-03-18 15:42:45 -0400

    added stubs to index.js

--- app/index.js
+++ app/index.js
@@ -2,9 +2,41 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
-  render: function(){
+  // Helper functions for our app
+
+  // getLocation should return the user's latitude and longitude
+  getLocation: function() {
+    // TODO
+    return {'lat': 0, 'long': 0};
+  };
+
+  // getPlaces should return the place id's of restaurants within {radius} of our user
+  getPlaces: function(location, radius) {
+    // TODO
+    return [1, 2, 3, 4, 5];
+  };
+
+  // getDetails should return the details of a place
+  getDetails: function(id) {
+    // TODO
+    return {'name': 'restaurant'};
+  };
+
+  render: function() {
+    var loc = getLocation();
+    var places = getPlaces();
+    var details = {};
+
+    for(place in places) {
+      details.push(getDetails(place));
+    }
+
     return(
-      <div>Hello World </div>
+      <div>
+        <h1>Restaurant Locator</h1>
+        <p>Location: {loc}</p>
+        <p>Details: {details}</p>
+      </div> 
     )
   }
 });
